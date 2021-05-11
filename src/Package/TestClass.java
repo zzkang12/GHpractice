498898d453f8ddc221c7a0d144ca30f8e0824a1f
kang <zzkang12@naver.com>
2021-05-11 11:02:06 +0900

    This is commit Message!

---
+++ src/Package/TestClass.java
@@ -0,0 +1,11 @@
+package Package;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello This is Github Practicing!");
+		//Github Practice!
+	}
+
+}
